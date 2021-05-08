dd71d2c3e9f457e0f463f2f1fd79443e9fde9192
MirAfzalul <mirafzalulbashar@gmail.comgit config --global user.email mirafzalulbashar@gmail.com>
2021-05-09 00:41:04 +0600

    Object and Constructors

---
+++ Object_Oriented/Creating Objects Function Constructors.js
@@ -0,0 +1,20 @@
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+    /*this.calAge = function() {
+        console.log(2021 - this.yearOfBirth);
+    }*/
+};
+Person.prototype.calculateAge = function() {
+    console.log(2021 - this.yearOfBirth);
+};
+var asif = new Person('Asif', '1995 ', 'Student');
+var anna = new Person('Anna', '2000', 'Painter');
+var mike = new Person('Mike', '1990', 'Teacher ');
+//asif.calAge();
+//anna.calAge();
+//mike.calAge();
+asif.calculateAge();
+anna.calculateAge();
+mike.calculateAge();
