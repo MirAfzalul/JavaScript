77ec4fa8864258820b6a506bd8db10acd3c6019f
MirAfzalul <mirafzalulbashar@gmail.comgit config --global user.email mirafzalulbashar@gmail.com>
2021-05-10 01:07:01 +0600

    object.create

--- Object_Oriented/Creating Objects Function Constructors.js
+++ Object_Oriented/Creating Objects Function Constructors.js
@@ -1,14 +1,20 @@
-var Person = function(name, yearOfBirth, job) {
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
-    /*this.calAge = function() {
-        console.log(2021 - this.yearOfBirth);
-    }*/
-};
+/*class Person {
+    constructor(name, yearOfBirth, job) {
+            this.name = name;
+            this.yearOfBirth = yearOfBirth;
+            this.job = job;
+            this.calAge = function() {
+                console.log(2021 - this.yearOfBirth);
+            }
+}
+// calculateAge() {
+//  console.log(2021 - this.yearOfBirth);
+//}
+}
 Person.prototype.calculateAge = function() {
     console.log(2021 - this.yearOfBirth);
 };
+Person.prototype.lastname = 'Mir';
 var asif = new Person('Asif', '1995 ', 'Student');
 var anna = new Person('Anna', '2000', 'Painter');
 var mike = new Person('Mike', '1990', 'Teacher ');
@@ -17,3 +23,23 @@ var mike = new Person('Mike', '1990', 'Teacher ');
 //mike.calAge();
 asif.calculateAge();
 anna.calculateAge();
+mike.calculateAge();*/
+
+//object.create 
+var personProto = {
+    calculateAge: function() {
+        console.log(2021 - this.yearofBirth);
+    }
+};
+var asif = Object.create(personProto);
+asif.name = 'Asif';
+asif.yearofBirth = '1995 ';
+asif.job = 'Student';
+var anna = Object.create(personProto, {
+    name: { value: 'Anna' },
+    yearofBirth: {
+        value: 2000
+    },
+    job: { value: 'Painter' }
+
+});
